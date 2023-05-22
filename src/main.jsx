9be12c5bbc389b1cc26b46e391318ae6901cc7e4
Mauricio Yair <yairchavez555@gmail.com>
2023-05-22 15:29:24 -0600

    [UPDATE] Add and update Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './components/Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Netflix Clone</h1>
+    <Header />
   </React.StrictMode>,
 )
