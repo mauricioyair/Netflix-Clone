49d9be3d1ab6bce700f92ecdc4e727a7f552f80b
Mauricio Yair <yairchavez555@gmail.com>
2023-05-24 12:46:05 -0600

    [UPDATE] Header semantic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import Logo from './../components/Logo'
 
 export default function Header() {
   return(
-    <div className="fixed flex justify-between bg-black text-white h-[24px] z-20 w-full py-[30px] px-[3%]">
+    <header className="fixed flex justify-between bg-black text-white h-[24px] z-20 w-full py-[30px] px-[3%]">
       <div className="flex flex-row items-center">
-        <a href="/" className="fill-[#e50914] inline-block leading-3 align-middle">
+        <a href="/" className="fill-color-primary inline-block leading-3 align-middle">
           <Logo />
         </a>        
       </div>
@@ -13,11 +13,11 @@ export default function Header() {
 
         <div className="mx-1.5">SERIES Y PELÍCULAS ILIMITADAS</div>
 
-        <button className="border-0 bg-[#e50914] rounded-[2px] font-normal leading-4 min-h-0 py-[9px] px-[20px] mx-1.5 hover:bg-[#f40612]" type="submit">SUSCRIBIRSE AHORA</button>
+        <button className="border-0 bg-color-primary rounded-[2px] font-normal leading-4 min-h-0 py-[9px] px-[20px] mx-1.5 hover:bg-color-hover" type="submit">SUSCRIBIRSE AHORA</button>
 
         <a href="#" className="border rounded-[2px] font-normal leading-4 min-h-0 py-[9px] px-[20px] mx-1.5 hover:bg-[hsla(0,0%,59%,.3)]">INICIAR SESIÓN</a>
 
       </div>
-    </div>
+    </header>
   )
 }
